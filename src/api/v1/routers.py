6b5c95917cfd04452bc8ccb7a7d2fcdc911e6818
yetian29 <dathien000@gmail.com>
2024-11-28 12:08:21 +0700

    Initial

--- src/api/v1/routers.py
+++ src/api/v1/routers.py
@@ -1,6 +1,5 @@
 from ninja import NinjaAPI
 
-from src.api.v1.cart.views import router as cart_router
 from src.api.v1.customer.views import router as customer_router
 from src.api.v1.product.views import router as product_router
 
@@ -8,4 +7,3 @@ api = NinjaAPI(docs_url="/api/v1/docs")
 
 api.add_router("/product", product_router, tags=["Product"])
 api.add_router("/customer", customer_router, tags=["Customer"])
-api.add_router("/cart", cart_router, tags=["Cart"])
